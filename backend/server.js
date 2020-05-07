01b6f262904e27487f9e812e2cb80d80e371a792
Joelle <joelle.maximin@gmail.com>
2020-05-07 12:24:14 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -20,16 +20,14 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-// if(process.env.NODE_ENV === 'production'){
-//   app.use(express.static('./build'));
-// }
+require('dotenv').config();
 
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
 	app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
 	app.get('*', function(req, res) {
-	  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
